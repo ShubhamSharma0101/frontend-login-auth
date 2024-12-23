2181184ad5e11dbbdf84e3413d3cd30ae4852c0d
ShubhamSharma0101 <shubhamsharma12532@gmail.com>
2024-12-23 22:39:40 +0530

    Handling any url

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import { BrowserRouter as Router, Route, Routes , Navigate} from "react-router-dom"
 import Login from "./componets/Login"
 import Home from "./componets/Home"
 import ProtectedRoute from "./componets/ProtectedRoute"
